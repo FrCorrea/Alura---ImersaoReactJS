19a6abe314386534d3e4ff08d778288ff97a4e0f
Francisco Correa <franciscocor.neto@gmail.com>
2022-01-28 11:53:13 -0300

    Aula 2 - UseState

--- .next/static/webpack/webpack.984b4080f583b10e.hot-update.js
+++ .next/static/webpack/webpack.984b4080f583b10e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "515740583aca3373"; }
+/******/ 	__webpack_require__.h = function() { return "dd416a5bf592853f"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/harmony module decorator */
---
+++ pages/chat.js
@@ -0,0 +1,6 @@
+
+export default function PaginaDoChat(){
+    return(
+        <>Página do chat</>
+    );
+}
